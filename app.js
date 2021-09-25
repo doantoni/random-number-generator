cf9369c0268ce15764ae4e423da76987e7625182
Dorian <65489913+doantoni@users.noreply.github.com>
2021-09-26 01:19:03 +0200

    Updated from old to new

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 let number = document.querySelector(".container")
-let exactNumber = document.querySelector("#exactNumber")
+let generatedNum = document.querySelector(".generatedNum")
+let input = document.querySelector("input")
+
+let digits = 1; 
+
+input.addEventListener("change", function(e){
+  digits = e.target.value;
+})
 
 let button = document.querySelector("button")
 
 button.addEventListener("click", function(){
-	let randNum = Math.floor(Math.random() * 100000000000000000 + 1);
+	let randNum = Math.floor(Math.floor(Math.random() * 10) * Math.pow(9, digits) / 10);
 	console.log(randNum)
-	exactNumber.innerHTML = randNum
+	generatedNum.innerHTML = randNum
 }) 
